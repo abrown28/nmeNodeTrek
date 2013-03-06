64236eb142efed13fc08d03a28040603a5b478c1
Alex Brown <alex.brown@rrd.com>
2013-03-06 16:25:10 -0600

    added player view

---
+++ Source/com/destructivegenius/nodetrek/graphics/PlayerView.hx
@@ -0,0 +1,20 @@
+
+package com.destructivegenius.nodetrek.graphics;
+
+import flash.display.Shape;
+
+class PlayerView extends Shape
+{
+    public function new()
+    {
+        super();
+        graphics.beginFill(0x000000);
+        graphics.moveTo(10, 0);
+        graphics.lineTo(-7, 7);
+        graphics.lineTo(-4, 0);
+        graphics.lineTo(-7, -7);
+        graphics.lineTo(10, 0);
+        graphics.endFill();
+    }
+
+}
