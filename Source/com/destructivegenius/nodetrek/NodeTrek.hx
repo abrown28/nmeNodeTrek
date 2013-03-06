a29cdc95144b9197d5968154f7b069f4571e98fd
Alex Brown <abrown28@gmail.com>
2013-03-06 06:46:17 -0600

    added a bunch of code copied from https://github.com/nadako/Ash-HaXe/blob/master/examples/asteroids/net/richardlord/asteroids/

--- Source/com/destructivegenius/nodetrek/NodeTrek.hx
+++ Source/com/destructivegenius/nodetrek/NodeTrek.hx
@@ -7,10 +7,17 @@ import ash.tick.ITickProvider;
 import ash.tick.FrameTickProvider;
 import ash.core.Engine;
 
+import com.destructivegenius.nodetrek.GameConfig;
+import com.destructivegenius.nodetrek.systems.GameManager;
+import com.destructivegenius.nodetrek.systems.SystemPriorities;
+import com.destructivegenius.nodetrek.systems.RenderSystem;
+
 class NodeTrek {
 
 	private var container:DisplayObjectContainer;
 	private var engine:Engine;
+    private var creator:EntityCreator;
+    private var config:GameConfig;
 
 	public function new(container:DisplayObjectContainer, width:Float, height:Float) {
         this.container = container;
@@ -19,25 +26,25 @@ class NodeTrek {
 
     private function prepare(width:Float, height:Float):Void {
         engine = new Engine();
-/*        
+        
         creator = new EntityCreator( engine );
-        keyPoll = new KeyPoll( container.stage );
+//        keyPoll = new KeyPoll( container.stage );
         config = new GameConfig();
         config.width = width;
         config.height = height;
 
         engine.addSystem(new GameManager( creator, config ), SystemPriorities.preUpdate);
-        engine.addSystem(new MotionControlSystem( keyPoll ), SystemPriorities.update);
-        engine.addSystem(new GunControlSystem( keyPoll, creator ), SystemPriorities.update);
-        engine.addSystem(new BulletAgeSystem( creator ), SystemPriorities.update);
-        engine.addSystem(new DeathThroesSystem( creator ), SystemPriorities.update);
-        engine.addSystem(new MovementSystem( config ), SystemPriorities.move);
-        engine.addSystem(new CollisionSystem( creator ), SystemPriorities.resolveCollisions);
-        engine.addSystem(new AnimationSystem(), SystemPriorities.animate);
+//        engine.addSystem(new MotionControlSystem( keyPoll ), SystemPriorities.update);
+//        engine.addSystem(new GunControlSystem( keyPoll, creator ), SystemPriorities.update);
+//        engine.addSystem(new BulletAgeSystem( creator ), SystemPriorities.update);
+//        engine.addSystem(new DeathThroesSystem( creator ), SystemPriorities.update);
+//        engine.addSystem(new MovementSystem( config ), SystemPriorities.move);
+//        engine.addSystem(new CollisionSystem( creator ), SystemPriorities.resolveCollisions);
+//        engine.addSystem(new AnimationSystem(), SystemPriorities.animate);
         engine.addSystem(new RenderSystem( container ), SystemPriorities.render);
 
         creator.createGame();
-*/
+
     }
 
     public function start():Void {
